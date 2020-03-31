33aeb3148cca257f300f30ee5fb7af064a0ba641
losanov <losanov@ukr.net>
2020-03-31 18:16:34 +0300

    add timer

--- js/script.js
+++ js/script.js
@@ -34,4 +34,57 @@ window.addEventListener('DOMContentLoaded', function () {
         }
 
     });
+
+    // Timer
+
+    let deadLine = '2020-04-01';
+
+    function getTimeRemainng(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)));
+
+            return {
+                'total' : t,
+                'hours' : hours,
+                'minutes' : minutes,
+                'seconds' : seconds
+            };
+    }
+
+    
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function addZero (num) {
+            if (num < 10 ) {
+                return '0' + num;
+            } else {
+                return num;
+            }
+        }
+
+        function updateClock () {
+            let t = getTimeRemainng(endtime);
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+
+    }
+
+    setClock('timer', deadLine);
 });
